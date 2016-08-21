dbd60203341b55e092730397197907ca525d5b8f
Jozadaque Batista <jozadaquebatista@gmail.com>
2016-08-20 23:43:12 -0300

    Update basic_stack.c

--- basic_stack.c
+++ basic_stack.c
@@ -2,31 +2,31 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct node {
+typedef struct node {
   char *el;
   struct node *n_node;
-};
+}NODE;
 
-_Bool push(char *v,struct node *n) {
+_Bool push(char *v,NODE *n) {
   if(n) {
-    push(n->next);
+    push(v,n->n_node);
   } else {
-    n->next = new(v);
+    n->n_node = new_node(v);
     return 0;
   }
 }
 
-_Bool pop(struct node *n) {
-  if(n->next) {
-    pop(n->next);
+_Bool pop(NODE *n) {
+  if(n->n_node) {
+    pop(n->n_node);
   } else {
     free(n);
     return 0;
   }
 }
-
-struct node *new(char *v) {
-  node *tmp = calloc(1,sizeof(node))
+ 
+NODE *new_node(char *v) {
+  NODE *tmp = calloc(1,sizeof(NODE));
         tmp->el = v;
         tmp->n_node = NULL;
         
@@ -34,13 +34,13 @@ struct node *new(char *v) {
 }
 
 int main(int argc, char **argv){
-  node *Stack = new("first el");
+   NODE *Stack = new_node("first el");
   
-  Stack->push("second el");
-  Stack->push("third el);
-  Stack->push("another el");
+  push("second el" ,Stack);
+  push("third el"  ,Stack);
+  push("another el",Stack);
   
-  Stack->pop();
+  pop(Stack);
   
   return 0;
 }
